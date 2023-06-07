5cedb01ff6fae8a45b57de6f37a4b30b4015422d
DanielBooysen <ST10145185@vcconnect.edu.za>
2023-06-07 16:40:31 +0200

    Added comments

--- MainProgram.cs
+++ MainProgram.cs
@@ -11,9 +11,15 @@ namespace ST10145185_DanielBooysen_PROG6211_POE
     internal class MainProgram
     {
         static List<Recipe> Recipes = new List<Recipe>();
+
+        //List variables used to store values until they are stored in the main list
         static List<string> IFoodGroup = new List<string>();
         static List<double> TempQuantity = new List<double>();
+
+        //Declaring delegate
         delegate int CheckTotalCalories(int num);
+
+        //Messages to print out in where needed
         static string Ast = "******************************";
         static string invInput = "Error: invalid input";
         static string prompt = "Please enter an operation -->";
@@ -21,9 +27,11 @@ namespace ST10145185_DanielBooysen_PROG6211_POE
         {
             Menu();
         }
+
+        //Menu method to display the menu to the user 
         public static void Menu()
         {
-            //int Option = 0;
+            //Variables used to store what is printed to the menu
             string WelcomeMsg = "Welcome to your recipe book!";
             string option1 = "1 - Enter recipe";
             string option2 = "2 - Display recipe";
@@ -34,7 +42,7 @@ namespace ST10145185_DanielBooysen_PROG6211_POE
             
             Console.ForegroundColor = ConsoleColor.Blue;
 
-            //Print out the menu 
+            //Print out the menu, center alligned
             Console.WriteLine();
             Console.WriteLine(String.Format("{0, " + (Console.WindowWidth / 2 + Ast.Length / 2) + "}", Ast));
             Console.WriteLine(String.Format("{0, " + (Console.WindowWidth / 2 + WelcomeMsg.Length / 2) + "}", WelcomeMsg));
@@ -52,6 +60,8 @@ namespace ST10145185_DanielBooysen_PROG6211_POE
             try
             {
                 int Option = Convert.ToInt32(Console.ReadLine());
+
+                //Switch to check user input and run appropriate method
                 switch (Option)
                 {
                     case 1:
@@ -94,11 +104,12 @@ namespace ST10145185_DanielBooysen_PROG6211_POE
 
             Console.Clear();
         }
-
+        //Method to delete a recipe
         private static void DeleteRecipe()
         {
             int option;
 
+            //Loops through all the recipe names and displays them to the user for them to choose from
             for (int i = 0; i < Recipes.Count; i++)
             {
                 Console.WriteLine(String.Format("{i + 1, " + (Console.WindowWidth / 2 + Recipes[i].RecipeName.Length / 2) + ", 0", Recipes[i].RecipeName));
@@ -106,8 +117,10 @@ namespace ST10145185_DanielBooysen_PROG6211_POE
 
             Console.WriteLine(String.Format("{0, " + (Console.WindowWidth / 2 + prompt.Length / 2) + "}", prompt));
 
+            //Try catch to ensure the user does not input an invalid value
             try
             {
+                //Removes the recipe specified by the user
                 option = int.Parse(Console.ReadLine());
                 option = option - 1;
                 Console.Clear();
@@ -124,7 +137,7 @@ namespace ST10145185_DanielBooysen_PROG6211_POE
                 DeleteRecipe();
             }            
         }
-
+        //Method to reset a recipe that has been scaled
         private static void ResetRecipe()
         {
             int option;
@@ -132,17 +145,20 @@ namespace ST10145185_DanielBooysen_PROG6211_POE
 
             Console.WriteLine(String.Format("{0, " + (Console.WindowWidth / 2 + prompt.Length / 2) + "}", prompt));
 
+            //Loops through all the recipe names and displays them to the user for them to choose from
             for (int i = 0; i < Recipes.Count; i++)
             {
                 Console.WriteLine(String.Format("{i + 1, " + (Console.WindowWidth / 2 + Recipes[i].RecipeName.Length / 2) + ", 0", Recipes[i].RecipeName));
             }
 
+            //Try catch to ensure the user does not input an invalid value
             try
             {
                 option = int.Parse(Console.ReadLine());
                 option = option - 1;
                 Console.Clear();
 
+                //Loop to assign the quantity value to a temporary variable so it can be restored
                 for (int i = 0; i < Recipes[option].IQuantity.Count; i++)
                 {
                     Recipes[option].IQuantity[i] = TempQuantity[i];
@@ -161,7 +177,7 @@ namespace ST10145185_DanielBooysen_PROG6211_POE
                 ResetRecipe();
             }
         }
-
+        //Method to scale the recipe
         private static void ScaleRecipe()
         {
             int option;
@@ -169,11 +185,13 @@ namespace ST10145185_DanielBooysen_PROG6211_POE
 
             Console.WriteLine(String.Format("{0, " + (Console.WindowWidth / 2 + prompt.Length / 2) + "}", prompt));
 
+            //Loop to diplay the recipe names for the user to choose from
             for (int i = 0; i < Recipes.Count; i++)
             {
                 Console.WriteLine(String.Format("{i + 1, " + (Console.WindowWidth / 2 + Recipes[i].RecipeName.Length / 2) + ", 0", Recipes[i].RecipeName));
             }
 
+            //Try catch to ensure the user does not input an invalid value
             try
             {
                 option = int.Parse(Console.ReadLine());
@@ -186,6 +204,7 @@ namespace ST10145185_DanielBooysen_PROG6211_POE
                 Console.WriteLine("3: 3");
                 scaleOption = int.Parse(Console.ReadLine());
 
+                //Loop to scale each ingredient to the relevant amount
                 for (int i = 0; i < Recipes[option].IQuantity.Count; i++)
                 {
                     TempQuantity.Add(Recipes[option].IQuantity[i]);
@@ -215,7 +234,7 @@ namespace ST10145185_DanielBooysen_PROG6211_POE
                 ScaleRecipe();
             }
         }
-
+        //Method to display recipe
         public static void DisplayRecipe()
         {
             int option;
@@ -225,12 +244,14 @@ namespace ST10145185_DanielBooysen_PROG6211_POE
 
             Console.WriteLine(String.Format("{0, " + (Console.WindowWidth / 2 + prompt.Length / 2) + "}", prompt));
 
+            //Loop to print out recipe name for the user to choose from
             foreach(var recipe in  Recipes) 
-                {
+            {
                     Console.WriteLine($"{Position + 1} {recipe.RecipeName}");
                     Position++;
-                }
+            }
 
+            //Try catch to ensure the user does not input an invalid value
             try
             {
                 option = int.Parse(Console.ReadLine());
@@ -244,6 +265,7 @@ namespace ST10145185_DanielBooysen_PROG6211_POE
                 Console.WriteLine(IngrMsg);
                 Console.WriteLine();
 
+                //Loop to print out relevant recipe
                 for (int i = 0; i < Recipes[option].IName.Count; i++)
                 {
                     Console.WriteLine(Recipes[option].IName[i]);
@@ -256,6 +278,7 @@ namespace ST10145185_DanielBooysen_PROG6211_POE
                 Console.WriteLine(StepsMsg);
                 Console.WriteLine();
 
+                //Loop to rpint out the steps in the relevant recipe
                 for (int i = 0; i < Recipes[option].StepDesc.Count; i++)
                 {
                     Console.WriteLine(Recipes[option].StepDesc[i]);
@@ -290,93 +313,115 @@ namespace ST10145185_DanielBooysen_PROG6211_POE
             int temp;
             string option;
 
-            Console.WriteLine("Enter the recipe name -->");
-            RecipeName = Console.ReadLine();
-            Console.WriteLine();
-
-            Console.WriteLine("Enter the number of ingredients in the recipe -->");
-            nrIngr = int.Parse(Console.ReadLine());
-            Console.WriteLine();
-
-            Console.WriteLine("Enter the number of steps in the recipe -->");
-            nrSteps = int.Parse(Console.ReadLine());
-            Console.WriteLine();
-
-            Console.Clear();
-
-            for (int i = 0; i < nrIngr; i++)
+            //Try catch to ensure the user does not input an invalid value
+            try
             {
-                Console.ForegroundColor = ConsoleColor.Green;
-                Console.WriteLine($"Ingredient number {i + 1}");
-                Console.ResetColor();
-                Console.WriteLine("Enter the ingredient name -->");
-                IName.Add(Console.ReadLine());
+                Console.WriteLine("Enter the recipe name -->");
+                RecipeName = Console.ReadLine();
                 Console.WriteLine();
 
-                Console.WriteLine("Enter the unit of measurement -->");
-                IUMeasure.Add(Console.ReadLine());
+                Console.WriteLine("Enter the number of ingredients in the recipe -->");
+                nrIngr = int.Parse(Console.ReadLine());
                 Console.WriteLine();
 
-                Console.WriteLine("Enter the quantity -->");
-                IQuantity.Add(int.Parse(Console.ReadLine()));
+                Console.WriteLine("Enter the number of steps in the recipe -->");
+                nrSteps = int.Parse(Console.ReadLine());
                 Console.WriteLine();
 
-                Console.WriteLine("Enter the number of calories in this ingredient -->");
-                temp = int.Parse(Console.ReadLine());
-                ICalories.Add(temp);
-                TotalCalories = TotalCalories + temp;
-                Console.WriteLine();
+                Console.Clear();
 
-                CheckTotalCalories CTC = new CheckTotalCalories(CheckCal);
-
-                if (CTC(TotalCalories) == -1)
+                //Loop to take uesr input for each ingredient
+                for (int i = 0; i < nrIngr; i++)
                 {
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine("Warning!");
-                    Console.WriteLine("The total calories exceed 300!");
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine($"Ingredient number {i + 1}");
                     Console.ResetColor();
-                    Console.WriteLine("Do you wish to continue? Y = Yes N = No");
-                    option = Console.ReadLine();
+                    Console.WriteLine("Enter the ingredient name -->");
+                    IName.Add(Console.ReadLine());
+                    Console.WriteLine();
 
-                    switch(option)
+                    Console.WriteLine("Enter the unit of measurement -->");
+                    IUMeasure.Add(Console.ReadLine());
+                    Console.WriteLine();
+
+                    Console.WriteLine("Enter the quantity -->");
+                    IQuantity.Add(int.Parse(Console.ReadLine()));
+                    Console.WriteLine();
+
+                    Console.WriteLine("Enter the number of calories in this ingredient -->");
+                    temp = int.Parse(Console.ReadLine());
+                    ICalories.Add(temp);
+                    TotalCalories = TotalCalories + temp;
+                    Console.WriteLine();
+
+                    //Create delegate
+                    CheckTotalCalories CTC = new CheckTotalCalories(CheckCal);
+
+                    //Runs delegate if the condition is met
+                    if (CTC(TotalCalories) == -1)
                     {
-                        case "Y":
-                            break;
-                        case "N":
-                            IName.Clear();
-                            IQuantity.Clear();
-                            IUMeasure.Clear();
-                            ICalories.Clear();
-                            TotalCalories = 0;
-                            StepDescr.Clear();
-                            IFoodGroup.Clear();
-                            Menu();
-                            break;
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Warning!");
+                        Console.WriteLine("The total calories exceed 300!");
+                        Console.ResetColor();
+                        Console.WriteLine("Do you wish to continue? Y = Yes N = No");
+                        option = Console.ReadLine();
+
+                        switch (option)
+                        {
+                            case "Y":
+                                break;
+                            case "N":
+                                IName.Clear();
+                                IQuantity.Clear();
+                                IUMeasure.Clear();
+                                ICalories.Clear();
+                                TotalCalories = 0;
+                                StepDescr.Clear();
+                                IFoodGroup.Clear();
+                                Menu();
+                                break;
+                        }
                     }
+
+                    EnterFoodGroup();
+                    Console.WriteLine();
+                    Console.Clear();
                 }
 
-                EnterFoodGroup();
-                Console.WriteLine();
-                Console.Clear();
-            }
+                //Loop to ask the user for the steps in the recipe
+                for (int i = 0; i < nrSteps; i++)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine($"Enter step number {i + 1}");
+                    Console.ResetColor();
+                    StepDescr.Add(Console.ReadLine());
+                    Console.WriteLine();
+                    Console.Clear();
+                }
 
-            for (int i = 0; i < nrSteps; i++)
+                Recipe recipe = new Recipe(RecipeName, IName, IQuantity, IUMeasure, ICalories, TotalCalories, IFoodGroup, StepDescr);
+
+                Recipes.Add(recipe);
+            }
+            catch
             {
-                Console.ForegroundColor = ConsoleColor.Green;
-                Console.WriteLine($"Enter step number {i + 1}");
-                Console.ResetColor();
-                StepDescr.Add(Console.ReadLine());
-                Console.WriteLine();
                 Console.Clear();
+                Console.WriteLine(String.Format("{0, " + (Console.WindowWidth / 2 + invInput.Length / 2) + "}", invInput));
+                IName.Clear();
+                IQuantity.Clear();
+                IUMeasure.Clear();
+                ICalories.Clear();
+                TotalCalories = 0;
+                StepDescr.Clear();
+                IFoodGroup.Clear();
+                EnterRecipe();
             }
-
-            Recipe recipe = new Recipe(RecipeName, IName, IQuantity, IUMeasure, ICalories, TotalCalories, IFoodGroup, StepDescr);
-
-            Recipes.Add(recipe);
 
             Menu();
         }
 
+        //Delegate method to check if the calories exceed 300
         public static int CheckCal(int num)
         {
             if (num > 300)
@@ -390,7 +435,7 @@ namespace ST10145185_DanielBooysen_PROG6211_POE
 
             return num;
         }
-
+        //Method to determine the food group that the ingredient belongs too
         public static void EnterFoodGroup()
         {
             int option;
